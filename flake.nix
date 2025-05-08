4a2e56d2ce9b6d87673563e141d27d15a6cd6419
Simon Gate <simon@kampgate.se>
2025-05-08 10:24:15 +0200

    Add eb-listener flake install

--- flake.nix
+++ flake.nix
@@ -3,22 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        buildInputs = (with pkgs; [
-          delve
-          go_1_22
-          goreleaser
-        ]);
-      };
-    }
-  );
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        eb-listener = pkgs.buildGoModule rec {
+          pname = "eb-listener";
+          version = "0.1.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "readly";
+            repo = "eb-listener";
+            rev = "v${version}";
+            #hash = pkgs.lib.fakeHash;
+            hash = "sha256-/OjKARWuTJPS8RTcOgXFFRO6Gdo/CvOEE1UvlWCcw8g=";
+          };
+          #vendorHash = pkgs.lib.fakeHash;
+          vendorHash = "sha256-W18cjpeJIFVWfq0lmVkaxGw0VVDxwrMG3Hxg2sTS7eE=";
+          meta = {
+            description = "";
+            homepage = "https://github.com/readly/eb-listener";
+            maintainers = with pkgs.maintainers; [ smgt ];
+          };
+        };
+      in {
+        packages = { default = eb-listener; };
+        devShells.default =
+          pkgs.mkShell { buildInputs = with pkgs; [ delve go goreleaser ]; };
+      });
 }
-
